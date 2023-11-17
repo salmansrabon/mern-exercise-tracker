f98e1bcfeead3f2dddc8b8648064daa46c32d5a1
salmansrabon <salmansrabon@gmail.com>
2023-11-17 12:24:21 +0600

    upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component.js";
+import EditExercises from "./components/edit-exercise.component.js";
+import CreateExercise from "./components/create-exercise.component.js";
+import CreateUsers from "./components/create-user.component.js";
+
+function App() {
+  return (
+
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Routes>
+          <Route path="/" element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercises />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUsers />} />
+        </Routes>
+      </div>
+    </Router>
+
+
+
+  );
+}
+
+export default App;
---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class EditExercises extends Component {
+  render() {
+    return (
+      <div>This is Edit exercise component</div>
+    )
+  }
+}
